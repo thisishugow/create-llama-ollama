868d594db78f3de313a6a6a80e233b3ab547ee74
thisishugow <59921505+thisishugow@users.noreply.github.com>
2024-02-15 23:38:08 +0100

    init for Chroma

--- backend/app/engine/generate.py
+++ backend/app/engine/generate.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,8 +8,9 @@ from app.engine.constants import DATA_DIR
 from app.engine.context import create_service_context
 from app.engine.utils import init_pg_vector_store_from_env
 from app.engine.loader import get_documents
+from app.storage import chroma_vector_store
 
-from llama_index import (
+from llama_index.core import (
     SimpleDirectoryReader,
     VectorStoreIndex,
     StorageContext,
@@ -18,11 +20,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def generate_datasource(service_context):
+def generate_datasource(service_context, from_:Literal["postgres", "chroma"]='chroma'):
     logger.info("Creating new index")
     # load the documents and create the index
     documents = get_documents()
-    store = init_pg_vector_store_from_env()
+
+    if from_ == 'postgres':
+        logger.info("Connecting to index from PGVector...")
+        store = init_pg_vector_store_from_env()
+    elif from_ == 'chroma':
+        logger.info("Connecting to index from ChromaDB...")
+        store = chroma_vector_store
+    else: 
+        ValueError(f"arg from_ must be in ['postgres', 'chroma']. Found='{from_}'")
+
     storage_context = StorageContext.from_defaults(vector_store=store)
     VectorStoreIndex.from_documents(
         documents,
@@ -31,7 +42,7 @@ def generate_datasource(service_context):
         show_progress=True,  # this will show you a progress bar as the embeddings are created
     )
     logger.info(
-        f"Successfully created embeddings in the PG vector store, schema={store.schema_name} table={store.table_name}"
+        f"Successfully created embeddings in the vector store, schema={store.schema_name} table={store.table_name}"
     )
 
 
